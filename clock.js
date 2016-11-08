6edbb0db41a03247a867cdcd149e769abf2c61b6
Kai Yu <kaichyu@gmail.com>
2016-11-07 18:11:11 -0800

    added another object constructor

--- clock.js
+++ clock.js
@@ -1,3 +1,7 @@
+/*8. Write a JavaScript program to create a Clock.
+Note : The output will come every second.
+Expected Console Output :
+*/
 function time() {
   let current = new Date();
   console.log(current.getHours() + ":" + current.getMinutes() + ":" + current.getSeconds());
