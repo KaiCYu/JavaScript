a03331b495215afd4c306a4117cfddbcabe4fe74
Kai Yu <kaichyu@gmail.com>
2016-11-08 17:27:43 -0800

    added interview question/solution, callback example

--- ex.js
+++ ex.js
@@ -1,25 +1,9 @@
-/*3. Write a JavaScript function to get the first element of an array.
-Passing a parameter 'n' will return the first 'n' elements of the array.
-Test Data :
-console.log(first([7, 9, 0, -2]));
-console.log(first([],3));
-console.log(first([7, 9, 0, -2],3));
-console.log(first([7, 9, 0, -2],6));
-console.log(first([7, 9, 0, -2],-3));
-Expected Output :
-7
-[]
-[7, 9, 0]
-[7, 9, 0, -2]
-[]
-*/
+let arr = [1,2,3,4,5,6,7];
 
-function first(arr, n) {
-	if (n == null) return arr[0];
-	return (n > 0) ? arr.slice(0,n) : [];
+function logIfEven(num) {
+	if( num%2 === 0) {
+		console.log(num);
+	}
 }
-console.log(first([7, 9, 0, -2]));
-console.log(first([],3));
-console.log(first([7, 9, 0, -2],3));
-console.log(first([7, 9, 0, -2],6));
-console.log(first([7, 9, 0, -2],-3));
+
+arr.forEach(logIfEven);
