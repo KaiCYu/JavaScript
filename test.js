827650ae41ba56c2bf4a3b1189799896f71e8ad5
KaiCYu <kaichyu@gmail.com>
2016-11-09 19:13:45 -0800

    finished chapter 2 on JS fundamental tutorials with notes

---
+++ test.js
@@ -0,0 +1,12 @@
+function reduce(array, combine, start) {
+  let current = start;
+
+  for (var i = 0; i < array.length; i++) {
+    current = combine( current, array[i]);
+  }
+  return current;
+}
+
+console.log(reduce([3,4,5,6,7], function(a, b) {
+  return a+b;
+},0));
