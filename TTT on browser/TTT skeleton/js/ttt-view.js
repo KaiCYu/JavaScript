2cbfe3026608720fd0c5697407c41e7a73aa92ca
KaiCYu <kaichyu@gmail.com>
2016-12-15 16:18:15 -0800

    finished tic tac toe

--- TTT on browser/TTT skeleton/js/ttt-view.js
+++ TTT on browser/TTT skeleton/js/ttt-view.js
@@ -4,17 +4,46 @@ class View {
     this.$el = $el;
 
     this.setupBoard();
+    this.bindEvents();
   }
 
   bindEvents() {
     this.$el.on('click', 'li', (event => {
-      const $square = $(event.currentTarget);
-      this.makeMove($square);
+      const $cell = $(event.currentTarget);
+      this.makeMove($cell);
     }));
   }
 
-  makeMove($square) {
-    alert('clicked');
+  makeMove($cell) {
+    const currentPlayer = this.game.currentPlayer;
+    const position = $cell.data('pos');
+
+    try {
+      this.game.playMove(position);
+    } catch (e) {
+      alert(`${e.msg}`);
+      return;
+    }
+
+    $cell.addClass(currentPlayer);
+    const winner = this.game.winner();
+    const $figcaption = $('<figcaption>');
+
+    if (this.game.isOver()) {
+
+      this.$el.off('click');
+
+      if (winner) {
+        this.$el.addClass(`winner-${winner}`);
+        $figcaption.html(`You win, ${winner}`);
+      } else {
+        $figcaption.html('It is a draw!');
+      }
+    }
+
+
+    this.$el.append($figcaption);
+
   }
 
   setupBoard() {
@@ -24,8 +53,8 @@ class View {
     for (let rowIdx = 0; rowIdx < 3; rowIdx++) {
       for (let colIdx = 0; colIdx < 3; colIdx++) {
         let $cell = $("<li>");
-        // $li.data("pos", [rowIdx, colIdx]);
-        $.data($cell, 'pos', [rowIdx, colIdx]);
+        $cell.data("pos", [rowIdx, colIdx]);
+        // $.data($cell, 'pos', [rowIdx, colIdx]);
         $board.append($cell);
       }
     }
