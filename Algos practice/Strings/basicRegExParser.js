4b5385675bfa5e3f87860fc16f6ac1be5b710fb0
KaiCYu <kaichyu@gmail.com>
2017-08-03 14:39:34 -0700

    implemented basic regex parser

--- Algos practice/Strings/basicRegExParser.js
+++ Algos practice/Strings/basicRegExParser.js
@@ -5,7 +5,7 @@ matches the pattern as a regular expression. For simplicity, assume that the act
 '.' and '*' do not appear in the text string and are used as special symbols only in the pattern string.
 
 In case you aren’t familiar with regular expressions, the function determines if the 
-text and pattern are the equal, where the '.' is treated as a single a character
+text and pattern are equal, where the '.' is treated as a single a character
 wildcard (see third example), and '*' is matched for a zero or more sequence of the
 previous letter (see fourth and fifth examples). For more information on regular expression matching, 
 see the Regular Expression Wikipedia page.
@@ -37,12 +37,43 @@ Constraints:
 */
 
 const isMatch = (text, pattern) => {
-  
+  return helper(text, pattern, 0, 0);
 }
 
+const helper = (text, pattern, textIndex, patIndex) => {
+  if (textIndex >= text.length) {
+    if (patIndex >= pattern.length) {
+      return true;
+    } else {
+      if (patIndex + 1 < pattern.length && (pattern[patIndex + 1] === '*')) {
+        return helper(text, pattern, textIndex, patIndex + 2);
+      } else {
+        false;
+      }
+    }
+  } else if (patIndex >= pattern.length && (textIndex < text.length)) {
+    return false;
+  } else if (patIndex + 1 < pattern.length && pattern[patIndex + 1] === '*') {
+    if (pattern[patIndex] === '.' || (text[textIndex] === pattern[patIndex])) {
+      return (helper(text, pattern, textIndex, patIndex + 2)) || 
+              helper(text, pattern, textIndex + 1, patIndex)
+    } else {
+      return helper(text, pattern, textIndex, patIndex + 2);
+    }
+  } else if (pattern[patIndex] === '.' || (pattern[patIndex] === text[textIndex])) {
+    return helper(text, pattern, textIndex + 1, patIndex + 1)
+  } else {
+    return false
+  }
+}
 
 console.log(isMatch('aa', 'a'));  //false
-console.log(isMatch('aa', 'a'));  //true
+console.log(isMatch('aa', 'aa'));  //true
 console.log(isMatch('abc', 'a.c'));   //true
 console.log(isMatch('abbb', 'ab*'));   //true
 console.log(isMatch('acd', 'ab*c.'));   //true
+
+/*
+time complexity: exponential, based length of the pattern
+same for space complexity
+*/
