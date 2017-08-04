dfd62fb3faba9813b1b8cf9a6b92c62a86ad438d
KaiCYu <kaichyu@gmail.com>
2017-08-03 17:06:01 -0700

    implemented bracket validator

---
+++ Algos practice/Stacks and Queues/bracketValidator.js
@@ -0,0 +1,51 @@
+/*
+We can ignore all letters besides the brackets. We use a stack to keep track of all open brackets. If we reach a closing bracket,
+we pop off from the stack, and obtain the matching opening bracket from a dictionary object, and checking it against the element that was 
+popped off. If they do not match, then we can return.
+
+After iterating through the string, if the stack is empty, we know all brackets have been matched, otherwise, not all brackets have matched.
+
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
+const bracketValidator = (string) => {
+  const openBrackets = {
+    '[': '[',
+    '{': '{',
+    '(': '(',
+  }
+
+  const closingBrackets = {
+    '}': '{',
+    ']': '[',
+    ')': '(',
+  }
+
+  let stack = [];
+
+  for (var i = 0; i < string.length; i++) {
+    if (openBrackets[string[i]]) {
+      stack.push(string[i]);
+    }
+    if (closingBrackets[string[i]]) {
+      let toCheck = stack.pop();
+      if (toCheck !== closingBrackets[string[i]]) {
+        return false
+      }
+    }
+  }
+  if (stack.length === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log(bracketValidator('(Hello World)') === true);
+console.log(bracketValidator('Hello World') === true); 
+console.log(bracketValidator('(Hello World)[') === false); 
+console.log(bracketValidator('Hello {W}orld') === true);
+console.log(bracketValidator('{Hello { W(orl[d]') === false);
+console.log(bracketValidator('H[el{lo}(Worl)d]') === true);
+console.log(bracketValidator('(H{ell[o Wo}rl]d)') === false );
+console.log(bracketValidator('[{(({{[Hell]o}}W)o)r}l](d)') === true);
