101d291a05ebdd118e116bfed6e394bdf9e267d5
KaiCYu <kaichyu@gmail.com>
2017-08-14 19:15:05 -0700

    added reverse string with recursion

--- Algos practice/Easy Interview Algos/reverse.js
+++ Algos practice/Easy Interview Algos/reverse.js
@@ -2,4 +2,14 @@ const reverseStr = (str) => {
   return str.split('').reverse().join('');
 }
 
+const reverseStrRec = (str) => {
+  if (str === '') {
+    return '';
+  }
+  return reverseStrRec(str.slice(1)) + str[0];
+}
+
 console.log(reverseStr('abcdef') === 'fedcba');
+console.log(reverseStr('') === '');
+console.log(reverseStrRec('abcdef') === 'fedcba');
+console.log(reverseStrRec('') === '');
